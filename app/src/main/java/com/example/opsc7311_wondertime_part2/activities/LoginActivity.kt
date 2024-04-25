8ad48d9c2697437dd869576f8d93a5effb9cfa07
Jaiden Naidoo <ST10218221@vcconnect.edu.za>
2024-04-25 14:07:30 +0200

    Login and Registration connected to firebase

--- app/src/main/java/com/example/opsc7311_wondertime_part2/activities/LoginActivity.kt
+++ app/src/main/java/com/example/opsc7311_wondertime_part2/activities/LoginActivity.kt
@@ -5,13 +5,57 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.opsc7311_wondertime_part2.R
+import com.example.opsc7311_wondertime_part2.databinding.ActivityLoginBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.LoginBtn.setOnClickListener{
+            val email =  binding.emailInput.text.toString()
+            val password =  binding.passwordInput.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty())
+            {
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if(task.isSuccessful)
+                        {
+                            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else
+                        {
+                            Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+            }
+            else
+            {
+                Toast.makeText(this, "Enter email and password", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+        binding.registerLink.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
 
         val login_btn: Button = findViewById(R.id.Login_btn)
         val register_link: TextView = findViewById(R.id.register_link)
--- app/src/main/java/com/example/opsc7311_wondertime_part2/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/opsc7311_wondertime_part2/activities/RegisterActivity.kt
@@ -5,12 +5,57 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.opsc7311_wondertime_part2.R
+import com.example.opsc7311_wondertime_part2.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth =  FirebaseAuth.getInstance()
+
+        binding.registerBtn.setOnClickListener{
+            val email =  binding.emailAdrdessInput.text.toString()
+            val password =  binding.passwordInput.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty())
+            {
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if(task.isSuccessful)
+                        {
+                            Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        else
+                        {
+                            Toast.makeText(this, "Registration Unsuccessful", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+            }
+            else
+            {
+                Toast.makeText(this, "Enter email and password", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+        binding.loginLink.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
 
         val register_btn: Button = findViewById(R.id.register_btn)
         val register_link: TextView = findViewById(R.id.login_link)
